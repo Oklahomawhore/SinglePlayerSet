23b76d2481d6cc98da5a531a482ca139c5ea7613
Oklahomawhore <Oklahomawhore@user.noreply.github.com>
2023-04-17 11:19:11 +0800

    Initial Commit

---
+++ SinglePlayerSet/SinglePlayerSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SinglePlayerSetApp.swift
+//  SinglePlayerSet
+//
+//  Created by Wangshu Zhu on 2023/4/17.
+//
+
+import SwiftUI
+
+@main
+struct SinglePlayerSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
